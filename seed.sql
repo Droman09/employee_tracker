3f26dda78fb91500bf1bb47e5720168722140615
David Roman <david_roman09@yahoo.com>
2021-02-06 09:55:09 -0600

    added schema file

--- seed.sql
+++ seed.sql
@@ -1,27 +1,32 @@
-DROP DATABASE IF EXISTS emp_trackerDB;
-CREATE database emp_trackerDB;
+INSERT INTO department (name) 
+values ("Sales"), ("Engineer"),("Finance"),("Legal");
 
-USE emp_trackerDB;
+INSERT INTO role (title, salary, department_id ) 
+values 
+('Sales Lead', 100000.00, 1),
+('Salesman', 80000.00, 1),
+('Lead Engineer', 150000.00, 2),
+('Software Engineer', 100000.00, 2),
+('Account Manager', 150000.00, 3),
+('Accountant', 100000.00, 3),
+('Legal Team Lead', 200000.00, 4),
+('Lawyer', 150000.00, 4);
 
-CREATE TABLE department (
-	id INT AUTO_INCREMENT NOT NULL,
-	name VARCHAR(30) NOT NULL,
-	PRIMARY KEY(id)
-);
 
-CREATE TABLE role (
-	id INT AUTO_INCREMENT NOT NULL ,
-	title VARCHAR(30) NOT NULL,
-	salary DECIMAL(10,2), 
-	department_id INT NOT NULL,
-    PRIMARY KEY(id)
-);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+values 
+('Blake', 'Daves', 1, NULL),
+('Carly', 'Wheatly', 2, 1),
+('Atwater', 'Carlson', 3, NULL),
+('Lee', 'Quinn', 4, 2),
+('Julia', 'Strickland', 5, NULL),
+('Adrian', 'Woods', 6, 3),
+('Alexis', 'Hum', 7, NULL);
 
-CREATE TABLE employee (
-	id INT AUTO_INCREMENT NOT NULL, 
-    first_name VARCHAR(30),
-    last_name VARCHAR(30), 
-    role_id INT NOT NULL, 
-    manager_id INT NULL,
-    PRIMARY KEY(id)
-);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
+
+
